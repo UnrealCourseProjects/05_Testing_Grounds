c6128df77d1aca2d62c151aae0d953493d17731c
Eric Holt <freydenkt@gmail.com>
2017-05-29 12:06:32 -0400

    Section 05 Lecture 221 - Introducing AI Perception part 2

--- S05_TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.h
+++ S05_TestingGrounds/Source/S05_TestingGrounds/ChooseNextWaypoint.h
@@ -18,4 +18,7 @@ class S05_TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
--- S05_TestingGrounds/Source/S05_TestingGrounds/PatrolRoute.h
+++ S05_TestingGrounds/Source/S05_TestingGrounds/PatrolRoute.h
@@ -5,24 +5,21 @@
 #include "Components/ActorComponent.h"
 #include "PatrolRoute.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+/**
+*A "route card" to help AI choose their next waypoint
+*/
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class S05_TESTINGGROUNDS_API UPatrolRoute : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UPatrolRoute();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+public:  
+	TArray<AActor*>GetPatrolPoints() const;
 	
+private:
+	//TODO remove or create Get
+	UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
+		TArray<AActor*>PatrolPoints;
+
+
 };
